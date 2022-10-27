325dff2d4a62baa2001e0e6461ac69dc04d1b5c1
Baxromxoja16 <aglamof@mail.ru>
2022-10-27 16:14:38 +0500

    feat: completed task
    delete from todo and add to "Complete"

--- app.js
+++ app.js
@@ -1,51 +1,119 @@
 // Create elements
 const newTask = document.querySelector("#new-task");
-const taskRowWrapper = document.querySelector(".task-row-wrapper");
-const tableList = document.querySelector("#incompleteTasks");
-const editBtn = document.querySelectorAll(".edit");
+const taskRowWrapper = document.querySelector(".task-row-wrapper button");
+const incompleteTasks = document.getElementById("incompleteTasks");
+const completedTasks = document.getElementById("completed-tasks");
 
-// Event handling, user interaction is what starts the code execution.
-taskRowWrapper.childNodes[1].addEventListener("click", addHandler);
-editBtn.forEach((x) => {
-  x.addEventListener("click", editHandler);
-});
+// Event handling
+taskRowWrapper.addEventListener("click", addTask);
 
-// add task
-function addHandler(e) {
-  e.preventDefault();
-  // Validation
-  if (newTask.value !== "") {
-    // create list 
-    tableList.innerHTML += `
-    <li>
-      <input type="checkbox">
-      <label class="task">${newTask.value}</label>
-      <input type="text" class="task" value=""${newTask.value}>
-      <button class="edit">Edit</button>
-      <button class="delete"><img src="./remove.svg"></button>
-    </li>
-    `;
-  }
+// Create tags
+function createNewTaskTags(labelText) {
+  let listTask = document.createElement("li");
+  // input chechbox
+  let inputCheckBox = document.createElement("input");
+  // label
+  let label = document.createElement("label");
+  // input text
+  let inputCheckText = document.createElement("input");
+  // button.edit
+  let btnEdit = document.createElement("button");
+
+  // delete button
+  let deleteBtn = document.createElement("button");
+  let deleteBtnImg = document.createElement("img");
+
+  inputCheckBox.type = "checkbox";
+
+  label.innerText = labelText;
+  label.className = "task";
+
+  inputCheckText.type = "text";
+  inputCheckText.className = "task";
+  inputCheckText.value = labelText
+
+  btnEdit.className = "edit";
+  btnEdit.innerText = "Edit";
+
+  deleteBtn.className = "delete";
+  deleteBtnImg.src = "./remove.svg";
+
+  deleteBtn.appendChild(deleteBtnImg);
+
+  // appending
+  listTask.appendChild(inputCheckBox);
+  listTask.appendChild(label);
+  listTask.appendChild(inputCheckText);
+  listTask.appendChild(btnEdit);
+  listTask.appendChild(deleteBtn);
+
+  return listTask;
 }
 
-// edit task
-function editHandler() {
-  let listItem = this.parentNode;
-  const taskLabel = listItem.querySelector(".task");
-  const taskInput = listItem.querySelector("input[type=text]");
+function addTask() {
+  if(!newTask.value) return
 
-  if (this.textContent === "Save") {
-    // change input
-    taskLabel.style.display = "block";
-    taskLabel.textContent = taskInput.value;
-    taskInput.style.display = "none";
-    // change btn text
-    this.textContent = "Edit";
-  } else if ("Edit") {
-    // change input
-    taskLabel.style.display = "none";
-    taskInput.style.display = "block";
-    // change btn text
-    this.textContent = "Save";
-  }
+  let taskValue = createNewTaskTags(newTask.value)
+  incompleteTasks.appendChild(taskValue)
+
+  bindTasks(taskValue,checkTaskTrue)
+
+  newTask.value = ""
 }
+
+function editTask() {
+
+  let listItem = this.parentNode
+
+  let editInput = listItem.querySelector("input[type=text]")
+  let editLabel = listItem.querySelector("label")
+  let editBtn = listItem.querySelector("button.edit")
+  
+  if(editBtn.innerText === "Edit"){
+    editBtn.innerText = "Save"
+  }else{
+    editBtn.innerText = "Edit"
+    editLabel.innerText = editInput.value
+  }
+
+  // add class(editMode) to tag li 
+  listItem.classList.toggle("editMode")
+}
+
+function deleteTask() {
+  this.parentNode.remove()
+}
+
+// checkbox true
+function checkTaskTrue() {
+  let listItem = this.parentNode
+  completedTasks.appendChild(listItem)
+  bindTasks(listItem,checkTaskFalse)
+}
+
+// checkbox false
+function checkTaskFalse() {
+  let listItem = this.parentNode
+  incompleteTasks.appendChild(listItem)
+  bindTasks(listItem,checkTaskTrue)
+}
+
+let bindTasks = function (taskItemList,checkTask) {
+  //edit task handler
+  taskItemList.children[3].onclick=editTask
+
+  // delete task handler
+  taskItemList.children[4].onclick=deleteTask
+
+  // check task handler
+  taskItemList.children[0].onchange=checkTask
+}
+
+for (let i = 0; i < incompleteTasks.children.length; i++) {
+  bindTasks(incompleteTasks.children[i],checkTaskTrue);
+}
+
+for (let i = 0; i < completedTasks.children.length; i++) {
+  bindTasks(completedTasks.children[i],checkTaskFalse);
+}
+
